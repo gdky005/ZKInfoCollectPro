93ac99f24b93f37abe4509016bf959948e2855b2
qingw <qingw@jumei.com>
2017-12-14 17:35:40 +0800

    1. 添加 BaseMVP。

---
+++ app/src/main/java/cc/zkteam/zkinfocollectpro/base/mvp/BaseMVPModule.java
@@ -0,0 +1,10 @@
+package cc.zkteam.zkinfocollectpro.base.mvp;
+
+/**
+ * MVP Module 基类
+ * Created by WangQing on 2017/12/2.
+ */
+
+public class BaseMVPModule {
+
+}
---
+++ app/src/main/java/cc/zkteam/zkinfocollectpro/base/mvp/BaseMVPView.java
@@ -0,0 +1,29 @@
+package cc.zkteam.zkinfocollectpro.base.mvp;
+
+/**
+ * MVP View 基类
+ * Created by WangQing on 2017/12/2.
+ */
+
+public interface BaseMVPView {
+
+    /**
+     * 加载中
+     */
+    void onLoading();
+
+    /**
+     * 无数据
+     */
+    void onNoData();
+
+    /**
+     * 网络请求结束
+     */
+    void onNetFinished();
+
+    /**
+     * 请求完成
+     */
+    void requestFinish();
+}
