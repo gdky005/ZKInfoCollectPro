e492a871662c8ba543d20e9126a862b80a7617a3
qingw <qingw@jumei.com>
2017-12-15 14:19:19 +0800

    1. 添加 HomeActivity。

---
+++ app/src/main/java/cc/zkteam/zkinfocollectpro/activity/home/HomeActivity.java
@@ -0,0 +1,31 @@
+package cc.zkteam.zkinfocollectpro.activity.home;
+
+import cc.zkteam.zkinfocollectpro.R;
+import cc.zkteam.zkinfocollectpro.base.BaseActivity;
+
+/**
+ * HomeActivity
+ * Created by WangQing on 2017/12/15.
+ */
+
+public class HomeActivity extends BaseActivity {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_home;
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
