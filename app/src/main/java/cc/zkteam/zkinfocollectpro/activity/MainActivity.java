0f42b7344488951b3ecb5dc15ae5555f44b6f5bb
qingw <qingw@jumei.com>
2017-12-21 15:22:38 +0800

    1. 更新请求权限。

--- app/src/main/java/cc/zkteam/zkinfocollectpro/activity/MainActivity.java
+++ app/src/main/java/cc/zkteam/zkinfocollectpro/activity/MainActivity.java
@@ -3,15 +3,14 @@ package cc.zkteam.zkinfocollectpro.activity;
 import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Environment;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.blankj.utilcode.util.PermissionUtils;
 import com.blankj.utilcode.util.ToastUtils;
 
 import java.net.URLEncoder;
@@ -41,7 +40,6 @@ public class MainActivity extends BaseActivity {
     private static final String TAG = "MainActivity";
 
 
-    private static final int REQUEST_EXTERNAL_STORAGE = 1;
     private static String[] PERMISSIONS_STORAGE = {
             Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.WRITE_EXTERNAL_STORAGE,
@@ -87,7 +85,6 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void initData() {
-
 
         verifyStoragePermissions(this);
     }
@@ -203,22 +200,18 @@ public class MainActivity extends BaseActivity {
     }
 
 
-    /**
-     * Checks if the app has permission to write to device storage
-     * If the app does not has permission then the user will be prompted to
-     * grant permissions
-     *
-     * @param activity
-     */
-    public static void verifyStoragePermissions(Activity activity) {
-        // Check if we have write permission
-        int permission = ActivityCompat.checkSelfPermission(activity,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+    public void verifyStoragePermissions(Activity activity) {
+        PermissionUtils.requestPermissions(activity, 200, PERMISSIONS_STORAGE, new PermissionUtils.OnPermissionListener() {
+            @Override
+            public void onPermissionGranted() {
 
-        if (permission != PackageManager.PERMISSION_GRANTED) {
-            // We don't have permission so prompt the user
-            ActivityCompat.requestPermissions(activity, PERMISSIONS_STORAGE,
-                    REQUEST_EXTERNAL_STORAGE);
-        }
+            }
+
+            @Override
+            public void onPermissionDenied(String[] deniedPermissions) {
+                ToastUtils.showShort("用户禁用了一些权限：" + deniedPermissions.toString());
+
+            }
+        });
     }
 }
