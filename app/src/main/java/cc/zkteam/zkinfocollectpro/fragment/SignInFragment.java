75ea8cda7ad8a2d7ab3edacf26a1a3878859be2f
qingw <qingw@jumei.com>
2017-12-24 00:05:04 +0800

    1. 修改我的问题上报的入口跳转。

--- app/src/main/java/cc/zkteam/zkinfocollectpro/fragment/SignInFragment.java
+++ app/src/main/java/cc/zkteam/zkinfocollectpro/fragment/SignInFragment.java
@@ -5,7 +5,9 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -16,9 +18,12 @@ import android.widget.Toast;
 import java.util.List;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 import butterknife.OnClick;
+import butterknife.Unbinder;
 import cc.zkteam.zkinfocollectpro.R;
 import cc.zkteam.zkinfocollectpro.activity.MainActivity;
+import cc.zkteam.zkinfocollectpro.activity.MyProblemListActivity;
 import cc.zkteam.zkinfocollectpro.api.ZHApi;
 import cc.zkteam.zkinfocollectpro.base.BaseFragment;
 import cc.zkteam.zkinfocollectpro.bean.ZHBaseBean;
@@ -82,6 +87,9 @@ public class SignInFragment extends BaseFragment {
     DrawerLayout drawerLayout;
     @BindView(R.id.test_btn)
     Button testBtn;
+    @BindView(R.id.tv_question_upload)
+    TextView tvQuestionUpload;
+    Unbinder unbinder;
 
     private ZHApi zhApi;
 
@@ -167,7 +175,7 @@ public class SignInFragment extends BaseFragment {
     }
 
     @OnClick({R.id.img_personal_info_about, R.id.img_personal_info_search, R.id.tv_sign, R.id.tv_sign_success_submit,
-            R.id.tv_about_message, R.id.tv_about_setting, R.id.tv_about_quit, R.id.test_btn})
+            R.id.tv_about_message, R.id.tv_about_setting, R.id.tv_about_quit, R.id.tv_question_upload, R.id.test_btn})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.img_personal_info_about:
@@ -187,6 +195,9 @@ public class SignInFragment extends BaseFragment {
             case R.id.tv_about_setting:
                 break;
             case R.id.tv_about_quit:
+                break;
+            case R.id.tv_question_upload:
+                PageCtrl.startActivity(getActivity(), MyProblemListActivity.class);
                 break;
             case R.id.test_btn:
                 PageCtrl.startActivity(getActivity(), MainActivity.class);
@@ -215,4 +226,17 @@ public class SignInFragment extends BaseFragment {
 
     }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // TODO: inflate a fragment view
+        View rootView = super.onCreateView(inflater, container, savedInstanceState);
+        unbinder = ButterKnife.bind(this, rootView);
+        return rootView;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
 }
