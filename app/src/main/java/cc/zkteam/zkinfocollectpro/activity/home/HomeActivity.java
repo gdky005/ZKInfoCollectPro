995d1a56e0dfae3d0bf0802cb34382afaf0eb02a
qingw <qingw@jumei.com>
2017-12-24 13:55:26 +0800

    1. 修改图片； 2. 添加动态权限。

--- app/src/main/java/cc/zkteam/zkinfocollectpro/activity/home/HomeActivity.java
+++ app/src/main/java/cc/zkteam/zkinfocollectpro/activity/home/HomeActivity.java
@@ -1,5 +1,7 @@
 package cc.zkteam.zkinfocollectpro.activity.home;
 
+import android.Manifest;
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -10,6 +12,9 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.MenuItem;
+
+import com.blankj.utilcode.util.PermissionUtils;
+import com.blankj.utilcode.util.ToastUtils;
 
 import javax.inject.Inject;
 
@@ -53,6 +58,17 @@ public class HomeActivity extends BaseActivity implements HasSupportFragmentInje
 
     public static int [] NAV_TYPE = new int[]{NAV_TYPE_MAIN, NAV_TYPE_DATA_COLLECT, NAV_TYPE_QUESTION_UPLOAD};
 
+
+    /**
+     * 添加 app 需要的动态权限
+     */
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_NETWORK_STATE,
+            Manifest.permission.CAMERA};
 
     @BindView(R.id.container_view_pager)
     ZKViewPager mViewPager;
@@ -138,7 +154,8 @@ public class HomeActivity extends BaseActivity implements HasSupportFragmentInje
 
     @Override
     protected void initData() {
-        testDI();
+//        testDI();
+        verifyStoragePermissions(this);
     }
 
     /**
@@ -203,6 +220,22 @@ public class HomeActivity extends BaseActivity implements HasSupportFragmentInje
     @Override
     public AndroidInjector<Fragment> supportFragmentInjector() {
         return supportFragmentInjector;
+    }
+
+
+    public void verifyStoragePermissions(Activity activity) {
+        PermissionUtils.requestPermissions(activity, 200, PERMISSIONS_STORAGE, new PermissionUtils.OnPermissionListener() {
+            @Override
+            public void onPermissionGranted() {
+
+            }
+
+            @Override
+            public void onPermissionDenied(String[] deniedPermissions) {
+                ToastUtils.showShort("您禁用了以下权限，app 可能无法正常运行：\n" + deniedPermissions.toString());
+
+            }
+        });
     }
 
 }
