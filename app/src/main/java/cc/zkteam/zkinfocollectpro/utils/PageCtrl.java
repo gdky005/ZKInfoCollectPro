3dd088fb5ff7172d8519f414e9648d06409f34cc
qingw <qingw@jumei.com>
2018-01-04 17:38:13 +0800

    1. 添加 跳转 到 新31项目页面的代码。

--- app/src/main/java/cc/zkteam/zkinfocollectpro/utils/PageCtrl.java
+++ app/src/main/java/cc/zkteam/zkinfocollectpro/utils/PageCtrl.java
@@ -4,7 +4,8 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 
-import cc.zkteam.zkinfocollectpro.ZKBase;
+import cc.zkteam.zkinfocollectpro.activity.BasicInfoActivity;
+import cc.zkteam.zkinfocollectpro.fragment.New31InfoFragment;
 
 /**
  * PageCtrl
@@ -31,6 +32,19 @@ public class PageCtrl {
         Intent intent = new Intent(context, cls);
         intent.putExtras(extra);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intent);
+    }
+
+    /**
+     * 启动 新31项 页面
+     * @param pageName  当前页面的名称：例如 人员信息
+     * @param pageType  当前页面的类型： 例如 renyuanxinxi_type
+     */
+    public static void startNew31InfoActivity(Context context, String pageName, String pageType) {
+        if (context == null) return;
+        Intent intent = new Intent(context, BasicInfoActivity.class);
+        intent.putExtra(New31InfoFragment.NEW_31_INFO_NAME_KEY, pageName);
+        intent.putExtra(New31InfoFragment.NEW_31_INFO_PAGE_TYPE_KEY, pageType);
         context.startActivity(intent);
     }
 
