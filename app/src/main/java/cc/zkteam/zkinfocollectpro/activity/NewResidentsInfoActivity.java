d18660648d2cbd75766bbef1da065f58f6dbbb46
mengxiaofeng <fengy0921@gmail.com>
2018-01-08 14:51:47 +0800

    1，民族只能输入汉字

--- app/src/main/java/cc/zkteam/zkinfocollectpro/activity/NewResidentsInfoActivity.java
+++ app/src/main/java/cc/zkteam/zkinfocollectpro/activity/NewResidentsInfoActivity.java
@@ -6,6 +6,8 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.text.InputFilter;
+import android.text.Spanned;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -88,6 +90,18 @@ public class NewResidentsInfoActivity extends BaseActivity {
     private ZHApi zkApi;
     private String r_type = "1";
 
+    InputFilter filter = new InputFilter() {
+        public CharSequence filter(CharSequence source, int start, int end,
+                                   Spanned dest, int dstart, int dend) {
+            for (int i = start; i < end; i++) {
+                if (!IdentityUtils.isChinese(source.charAt(i))) {
+                    return "";
+                }
+            }
+            return null;
+        }
+    };
+
     @Override
     protected int getLayoutId() {
 
@@ -128,6 +142,7 @@ public class NewResidentsInfoActivity extends BaseActivity {
             }
         });
 
+        nationaledittext.setFilters(new InputFilter[]{filter});
     }
 
     @Override
